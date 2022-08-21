db50d4cd5dccf14e1609a3426ec065d43abd059c
Ruslan Gureev <ruslanus1989@gmail.com>
2022-08-21 11:23:35 +0300

    Zadanie4 done

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= n )
+{
+    if (i%2 ==0)
+    {
+      Console.Write(i.ToString() + " ");
+    i++;   
+    }
+    else
+    i++;
+}
