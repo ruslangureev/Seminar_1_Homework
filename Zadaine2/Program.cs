95f4e3b54c3629be286bffdae3de62148c785db0
Ruslan Gureev <ruslanus1989@gmail.com>
2022-08-21 10:00:54 +0300

    Zadanie2 done

---
+++ Zadaine2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a>b && a>c)
+{
+    Console.WriteLine("Max= " + a );
+}
+else if (b>a && b>c)
+{
+    Console.WriteLine("Max= " + b );
+}
+else
+{
+    Console.WriteLine("Max= " + c );
+}
