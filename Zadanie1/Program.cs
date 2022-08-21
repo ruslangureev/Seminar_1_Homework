9a1303b74b796c085a970ed18d96d3087cb922d4
Ruslan Gureev <ruslanus1989@gmail.com>
2022-08-21 09:49:11 +0300

    Zadanie1 done

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int b = int.Parse(Console.ReadLine());
+if (a < b)
+{
+    Console.WriteLine("Max = " + " " + b + "," + " " + "Min=" + " " + a );
+}
+
+else if(a > b)
+{
+    Console.WriteLine("Max = " + " " + a + "," + " " + "Min=" + " " + b );
+  
+}
+else
+{
+    Console.WriteLine("Значения равны");
+}
+
+
